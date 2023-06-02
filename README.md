22562d9988b799c5840fa4dbd810769013cdb21d
Waldo Sobrino <waldo2810@gmail.com>
2023-06-01 23:04:36 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# My nvim config
+
+This was inspire by (adibhanna)[https://github.com/adibhanna/lvim-config]'s version.
+
+Note: that this is a WIP.
---
+++ lua/config/keymaps.lua
@@ -0,0 +1,48 @@
+local opts = { noremap = true, silent = true }
+
+local map = vim.keymap.set
+
+-- map("n", "<C-Space>", ":WhichKey \\<space><cr>", opts)
+map("n", "<C-d>", "<C-d>zz", opts)
+map("n", "<C-u>", "<C-u>zz", opts)
+map("v", "J", ":m '>+1<CR>gv=gv", opts)
+map("v", "K", ":m '<-2<CR>gv=gv", opts)
+-- map("n", "<leader><leader>", ":Telescope buffers<CR>", opts)
+
+-- Fast saving
+map("n", "<Leader>w", ":write!<CR>", opts)
+map("n", "<Leader>q", ":q!<CR>", opts)
+
+-- Remap for dealing with visual line wraps
+map("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true })
+map("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true })
+
+-- better indenting
+map("v", "<", "<gv")
+map("v", ">", ">gv")
+
+-- paste over currently selected text without yanking it
+map("v", "p", '"_dp')
+map("v", "P", '"_dP')
+
+-- copy everything between { and } including the brackets
+-- p puts text after the cursor,
+-- P puts text before the cursor.
+map("n", "YY", "va{Vy", opts)
+
+-- Exit on jj and jk
+map("n", "j", "gj", opts)
+map("n", "k", "gk", opts)
+
+-- Exit on jj and jk
+map("i", "jj", "<ESC>", opts)
+map("i", "jk", "<ESC>", opts)
+
+-- Fast saving
+map("n", "<Leader>w", ":write!<CR>", opts)
+map("n", "<Leader>q", ":q!<CR>", opts)
+
+-- Navigate buffers
+map("n", "<S-l>", ":bnext<CR>", opts)
+map("n", "<S-h>", ":bprevious<CR>", opts)
+
